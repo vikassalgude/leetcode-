64e66a77dca98b86c0399c00f1ccbc090d689d12
VIKAS SALGUDE <142891781+vikassalgude@users.noreply.github.com>
2025-03-27 23:36:41 +0530

    Create 538. Convert BST to Greater Tree.cpp

---
+++ DSA/538. Convert BST to Greater Tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void helper(TreeNode* root, int &sum) {
+        if(!root)return;
+        helper(root->right,sum);
+        sum+=root->val;
+        root->val=sum;
+        helper(root->left,sum);
+
+    }
+    
+
+    TreeNode* convertBST(TreeNode* root) {
+        int sum = 0;
+        helper(root,sum);
+        return root;
+    }
+};
